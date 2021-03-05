e6f4a867155e5acda8e4e1a2177d213a1503de96
Przemysław Matkowski <matkowski1994@gmail.com>
2021-03-05 20:00:24 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: "PG-Clima",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+  ],
+};
